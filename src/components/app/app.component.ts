6ddfa76781ce7226f3fbe0a8e0a25d348176193e
Noel Abellanosa <abellnoel99@gmail.com>
2023-02-05 11:12:55 -0500

    feat: add ng-bootstrap and test component

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbAlert],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  show = true;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { Routes, provideRouter } from '@angular/router';
+import '@angular/localize/init';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
