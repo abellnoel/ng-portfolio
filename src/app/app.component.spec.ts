84473f1c3d9ead53e6809b83fc52a6f813901526
Noel Abellanosa <abellnoel99@gmail.com>
2023-02-04 20:20:08 -0500

    feat: add changelog, release, deploy process

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-portfolio'`, () => {
+  it(`should have as title 'Abellnoel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-portfolio');
+    expect(app.title).toEqual('Abellnoel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Abellnoel app is running!'
+    );
   });
 });
