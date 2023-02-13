763e9d794981e3d3f2c18c36540087b4a3fbd386
Noel Abellanosa <abellnoel99@gmail.com>
2023-02-12 20:01:35 -0500

    fix: add RouterTestingModule to search specs

--- src/components/searchbar/searchbar.component.spec.ts
+++ src/components/searchbar/searchbar.component.spec.ts
@@ -1,23 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchbarComponent } from './searchbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SearchbarComponent', () => {
-  let component: SearchbarComponent;
-  let fixture: ComponentFixture<SearchbarComponent>;
+    let component: SearchbarComponent;
+    let fixture: ComponentFixture<SearchbarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ SearchbarComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [SearchbarComponent, RouterTestingModule],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(SearchbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SearchbarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
